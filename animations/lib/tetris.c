e93b23031125bfe7207579afdd40fa0d46f43ab2
sebseb7 <github@seb.exse.net>
2012-11-09 05:59:15 +0100

    simple tetris demo with 3 bots

---
+++ animations/lib/tetris.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "grid.h"
+#include "tetris.h"
+
+enum {
+	MAX_PLAYERS = 3
+};
+
+static Grid grids[MAX_PLAYERS];
+
+
+void tetris_load() {
+	int i;
+	for(i = 0; i < MAX_PLAYERS; i++) init_grid(&grids[i],i);
+}
+
+void tetris_update() {
+	int i;
+	for(i = 0; i < MAX_PLAYERS; i++) {
+		update_grid(&grids[i]);
+		draw_grid(&grids[i]);
+	}
+}
+
+int add_player() {
+	int i;
+	for(i = 0; i < MAX_PLAYERS; i++) {
+		if(activate_grid(&grids[i])) return i;
+	}
+
+	return -1;
+}
+int get_lines(int nr)
+{
+	return grids[nr].lines;
+}
+void remove_player(int nr) {
+	init_grid(&grids[nr], nr);
+}
+
+draw_cb drawfunc;
+
+void setDrawCb(draw_cb draw_func)
+{
+	drawfunc = draw_func;
+}
+
+
+void pixel(int x, int y, unsigned char color)
+{
+	drawfunc(x,y,(int)color);
+}
+
---
+++ animations/tetris.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+#include "lib/tetris.h"
+#include "libs/text.h"
+
+void pixeldraw(int x, int y, int color)
+{
+	uint8_t r[] = {0,25,255,255,127,127,0,  0  ,0  ,0  ,127,127,0  ,0  ,0  ,255};
+	uint8_t g[] = {0,0  ,255,0  ,0  ,255,127,0  ,255,255,127,255,127,127,0  ,255};
+	uint8_t b[] = {0,0  ,0  ,255,255,0  ,255,255,255,127,255,0  ,127,0  ,127,255};
+
+	x+=3;
+	y+=3;
+
+	setLedXY(x*3,y*3,r[color],g[color],b[color]);
+	setLedXY(x*3+1,y*3+1,r[color],g[color],b[color]);
+	setLedXY(x*3,y*3+1,r[color],g[color],b[color]);
+	setLedXY(x*3+1,y*3,r[color],g[color],b[color]);
+}
+
+static void init(void)
+{
+	setDrawCb(pixeldraw);
+	tetris_load();
+}
+
+static void deinit(void)
+{
+}
+
+
+static uint8_t tick(void) {
+
+
+	tetris_update();
+
+	for(int i = 0; i < 3; i++)
+	{
+		int lines = get_lines(i);
+		fill_8x6(i * 36 +15,5, 3,0,0,0);
+		draw_number_8x6(i * 36 + 15,5, lines, 3, ' ' ,255,255,255);
+	}
+
+	return 0;
+}
+
+int button_down(unsigned int nr, unsigned int button)
+{
+	return 0;
+}
+int is_occupied(unsigned int nr)
+{
+	return 0;
+}
+
+
+
+static void constructor(void) CONSTRUCTOR_ATTRIBUTES
+void constructor(void) {
+	registerAnimation(init,tick,deinit, 4, 3000);
+}
+
