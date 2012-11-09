4fd22b9d1c4d63b30a407b6e5af041eaa0963b63
sebseb7 <github@seb.exse.net>
2012-11-09 05:57:49 +0100

    added fill_8x6

--- firmware/libs/text.h
+++ firmware/libs/text.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+void fill_8x6(uint8_t x, uint8_t y, uint8_t count,uint8_t r, uint8_t g , int8_t b);
 void draw_text_8x6(uint8_t x, uint8_t y, const char *text, uint8_t r,uint8_t g,uint8_t b);
 void draw_text_inv_8x6(uint8_t x, uint8_t y, const char *text);
 void draw_number_8x6(uint8_t x, uint8_t y, int32_t number, uint8_t length, uint8_t pad, uint8_t r, uint8_t g , uint8_t b);
