7bfca88e5f1b3243f72c5aef1b54f6cb5ad6b3d3
Astro <astro@spaceboyz.net>
2012-12-24 01:39:51 +0100

    animations/qr_clock, import generator code from https://github.com/zint/zint

---
+++ animations/qr_clock.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#include "main.h"
+#include "libs/armmath.h"
+#include "libs/zint/zint.h"
+#include "libs/zint/common.h"
+
+#define BORDER 3
+int qr_code(struct zint_symbol *symbol, unsigned char source[], int length);
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+static void init(void) {
+    lcdFillRGB(255, 255, 255);
+}
+
+static void deinit(void) {
+}
+
+static uint8_t tick(void) {
+    int time = getSysTick() / 10000;
+    char text[128];
+    snprintf(text, 127, "Time: %i", time);
+
+    struct zint_symbol zs;
+    memset(&zs, 0, sizeof(zs));
+    zs.input_mode = DATA_MODE;
+    zs.option_1 = 3;
+    /* zs.option_2 = 2; */
+    int qr_res = qr_code(&zs, text, strlen(text));
+    if (qr_res) {
+        /* printf("Error: %i\n", qr_res); */
+    }
+
+    int zoom = MAX(1, MIN(LED_WIDTH / (zs.width + (BORDER * 2)), LED_HEIGHT / (zs.rows + (BORDER * 2))));
+
+    int x0 = (LED_WIDTH - zoom * zs.width) / 2;
+    int y0 = (LED_HEIGHT - zoom * zs.rows) / 2;
+    for(int y = 0; y < zs.width; y++) {
+        for(int x = 0; x < zs.rows; x++) {
+            for(int y1 = y * zoom; y1 < (y + 1) * zoom; y1++) {
+                for(int x1 = x * zoom; x1 < (x + 1) * zoom; x1++) {
+                    if (module_is_set(&zs, x, y)) {
+                        setLedXY(x0 + x1, y0 + y1, 0, 0, 0);
+                    } else {
+                        setLedXY(x0 + x1, y0 + y1, 255, 255, 255);
+                    }
+                }
+            }
+        }
+    }
+        
+    time++;
+    return 0;
+}
+
+static void constructor(void) CONSTRUCTOR_ATTRIBUTES
+    void constructor(void) {
+    registerAnimation(init,tick,deinit, 1000, 2000);
+}
