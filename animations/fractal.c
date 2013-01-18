c89ec0fa33809e44b8a037ef76a7ed0d8fa79d5c
Timo Fuckner <omg-lachmoewe@gmx.net>
2013-01-18 13:31:59 +0100

    beautifulisation of code

--- animations/fractal.c
+++ animations/fractal.c
@@ -5,14 +5,15 @@
 #include "libs/armmath.h"
 //#include "lib/sektor.h"
 
-#define MAXITER 120
+#define MAXITER 150
 #define RADIUS 4
 #define ZOOMSPEED 0.99
 static float Maxx=0.5;             // Rightmost Real point of plane to be displayed
 static float Minx=-2;            // Leftmost Real point
 static float Maxy=1.25;             // Uppermost Imaginary point
 static float Miny=-1.25;            // Lowermost Imaginary point
-
+static float PoIx=-0.74977879167273;
+static float PoIy=0.061208886981615;
 float dx=0;
 float dy=0;
 
@@ -40,12 +41,13 @@ static void mandel(uint8_t xin, uint8_t yin, uint8_t *r, uint8_t *g, uint8_t *b)
 				float y=0;		//converting from pixels to points
 				float xnew=0;
 				uint8_t k=0;
-
+				float offsetx = xin*dx+Minx;
+				float offsety = Maxy-yin*dy;
 				while(k<=MAXITER)  // Each pixel loop
 				{
 								//The Mandelbrot Function Z=Z*Z+c into x and y parts
-								xnew=x*x-y*y + xin*dx+Minx;
-								y=2*x*y   + Maxy-yin*dy;
+								xnew=x*x-y*y + offsetx;
+								y=2*x*y   + offsety;
 								x=xnew;
 								k++;
 								if ( (x*x+y*y)>RADIUS ) break;  // Break condition
@@ -54,39 +56,39 @@ static void mandel(uint8_t xin, uint8_t yin, uint8_t *r, uint8_t *g, uint8_t *b)
 				uint8_t color = k;
 				if (color>15) color=color%15;
 				if (k>MAXITER) {
-					*r=0;
-					*g=0;
-					*b=0;
+								*r=0;
+								*g=0;
+								*b=0;
 				} else {
-					*r=colors[color][0];
-					*g=colors[color][1];
-					*b=colors[color][2];
+								*r=colors[color][0];
+								*g=colors[color][1];
+								*b=colors[color][2];
 				}
 }
 
 static uint8_t tick(void) {
 
-	 dx=(Maxx-Minx)/LED_HEIGHT;
-	 dy=(Maxy-Miny)/LED_WIDTH;
+				dx=(Maxx-Minx)/LED_HEIGHT;
+				dy=(Maxy-Miny)/LED_WIDTH;
 
-	uint8_t x,y,r,g,b;
-	uint8_t *rp = &r; 
-	uint8_t *gp = &g;
-	uint8_t *bp = &b;
-	for(y = 0; y < LED_HEIGHT; y++) 
-	{
-		for(x = 0; x < LED_WIDTH; x++) 
-		{
-			mandel(x,y,rp,gp,bp);
-			setLedXY(x,y,r,g,b);
-		}
-	}
-	Maxx=Maxx*ZOOMSPEED;
-	Minx=Minx*ZOOMSPEED;
-	Maxy=Maxy*ZOOMSPEED;
-	Miny=Miny*ZOOMSPEED;
+				uint8_t x,y,r,g,b;
+				uint8_t *rp = &r; 
+				uint8_t *gp = &g;
+				uint8_t *bp = &b;
+				for(y = 0; y < LED_HEIGHT; y++) 
+				{
+								for(x = 0; x < LED_WIDTH; x++) 
+								{
+												mandel(x,y,rp,gp,bp);
+												setLedXY(x,y,r,g,b);
+								}
+				}
+				Maxx=Maxx*ZOOMSPEED+PoIx*(1-ZOOMSPEED);
+				Minx=Minx*ZOOMSPEED+PoIx*(1-ZOOMSPEED);
+				Maxy=Maxy*ZOOMSPEED+PoIy*(1-ZOOMSPEED);
+				Miny=Miny*ZOOMSPEED+PoIy*(1-ZOOMSPEED);
 
-	return 0;
+				return 0;
 }
 
 
@@ -103,7 +105,7 @@ static void deinit(void)
 
 static void constructor(void) CONSTRUCTOR_ATTRIBUTES
 void constructor(void) {
-	registerAnimation(init,tick,deinit, 4, 1638); 
+				registerAnimation(init,tick,deinit, 4, 1638); 
 }
 
 
