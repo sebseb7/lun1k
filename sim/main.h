be39bd5b8b2a04512dcbbaba18a9d80b01a99bc6
sebseb7 <github@seb.exse.net>
2013-02-15 06:50:47 +0100

    merge

--- sim/main.h
+++ sim/main.h
@@ -44,5 +44,7 @@ void setLedXY(uint8_t x,uint8_t  y, uint8_t r,uint8_t g,uint8_t b);
 void invLedXY(uint8_t x,uint8_t  y);
 void getLedXY(uint8_t x, uint8_t y, uint8_t* red,uint8_t* green, uint8_t* blue);
 void lcdFillRGB(uint8_t r,uint8_t g , uint8_t b);
+
+
 #endif
 
